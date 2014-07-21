6302eb46350587371e459b5fba256c65c2241e3d
Andrew Jenkins <andrewjjenkins@gmail.com>
2014-07-21 11:17:57 -0600

    /tweets handler that returns raw JSON

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,7 +1,13 @@
 package controllers
 
 import play.api._
+import play.api.Play.current
+import play.api.libs.json.{JsObject, JsValue, Json, __}
 import play.api.mvc._
+import play.api.libs.ws.WS
+
+import scala.concurrent.Future
+import scala.concurrent.ExecutionContext.Implicits.global
 
 object Application extends Controller {
 
@@ -9,4 +15,16 @@ object Application extends Controller {
     Ok(views.html.index("This is a different message"))
   }
 
+  def search(query: String) = Action.async {
+    fetchTweets(query).map(tweets => Ok(tweets))
+  }
+
+  def fetchTweets(query: String): Future[JsValue] = {
+    val tweetsFuture = WS.url("http://search-twitter-proxy.herokuapp.com/search/tweets").withQueryString("q" -> query).get()
+    tweetsFuture.map {
+      response => response.json
+    } recover {
+      case _ => Json.obj("responses" -> Json.arr())
+    }
+  }
 }
